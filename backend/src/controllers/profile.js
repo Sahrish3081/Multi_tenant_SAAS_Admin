39c67a660863ce306bbf3aae4b22bf9f3e2ff567
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-08-21 11:07:55 +0500

    feat: add profile controller

--- backend/src/controllers/profile.js
+++ backend/src/controllers/profile.js
@@ -1,7 +1,37 @@
+import { db } from "#config/client.js";
+import { users } from "#drizzle/schema.js";
+import { eq } from "drizzle-orm";
 export function profileController(req, res) {
-    return res.status(200).json({
-        success: true,
-        message: "Profile accessed successfully",
-        user: req.user
+    const userId=req.user.id;
+    try{
+
+       const user=await db.select({
+        name:users.name,
+        email:users.email
+       }).from(users)
+       .where(eq(users.id,userId))
+       .limit(1) ;
+       
+       if(user.length===0){
+        res.status(404).json({
+            message:"User not found:"
+        })
+       }
+       return res.status(200).json({
+        user: user[0],
     });
+    }
+    catch(error){
+      
+    console.error("Get profile error:", error);
+
+    return res.status(500).json({
+      message: "Failed to fetch profile",
+    });
+     
+    }
+    
+    
+    
+    
 }
